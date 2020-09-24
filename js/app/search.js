26b4c1d2e63217de04caf776a4f5c67496a5926a
HermadesCGCO <saraaahinreallife@gmail.com>
2020-09-24 16:25:22 +0000

    Working on searching

---
+++ js/app/search.js
@@ -0,0 +1,38 @@
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1)
+  var vars = query.split("&")
+  for(var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=")
+    if(pair[0] === variable) {
+      return decodeURIComponent(pair[1].replace(/\+/g, "%20"))
+    }
+  }
+}
+
+function returnSearchData(callback) {
+  var file = new XMLHttpRequest()
+  file.open("GET", "/search.json", false)
+  file.onreadystatechange = function() {
+    if(file.readyState === 4) {
+      if(file.status === 200 || file.status === 0) {
+        var text = file.responseText
+        callback(text)
+      }
+    }
+  }
+  file.send(null)
+}
+
+function getObjects(object, key, val) {
+  var objects = []
+  for(var i in object) {
+    if(!object.hasOwnProperty(i)) continue
+    if(typeof object[i] === 'object') {
+      objects = objects.concat(getObjects(object[i], key, val))
+    } else if(i === key && (object[key].toLowerCase().includes(term.toLowerCase()))) {
+      objects.push(object)
+    }
+  }
+
+  return objects
+}
